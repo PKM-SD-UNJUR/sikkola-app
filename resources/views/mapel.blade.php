0edccba21493fcf94f5a95ae2233821f23dfe793
rizki-P-star24 <83244066+rizki-P-star24@users.noreply.github.com>
2022-07-08 23:25:43 +0700

    reapir

--- resources/views/mapel.blade.php
+++ resources/views/mapel.blade.php
@@ -1,79 +1,13 @@
 @extends('template.main')
 
 @section('container')
-<div class="container mapelcaption text-center card-kelas" style="width: 80%; padding: 30px; border-radius: 5px; background-image: url('{{ asset('gambar/chalkboard.jpg') }}');" >
-  <h2>{{$mapel->kelas->nama}}</h2>
-  <h1>{{$mapel->nama}}</h1>
-<style>
-#clock {
-    text-align: center;
-}
-</style>
-<div id="clock" class="mb-1"><h5>8:10:45</h5></div>
-<script>
-setInterval(showTime, 1000);
-function showTime() {
-    let time = new Date();
-    let hour = time.getHours();
-    let min = time.getMinutes();
-    let sec = time.getSeconds();
-    am_pm = "AM";
 
-    if (hour > 12) {
-        hour -= 12;
-        am_pm = "PM";
-    }
-    if (hour == 0) {
-        hr = 12;
-        am_pm = "AM";
-    }
-
-    hour = hour < 10 ? "0" + hour : hour;
-    min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
-
-    let currentTime = hour + ":" 
-        + min + ":" + sec + am_pm;
-
-    document.getElementById("clock")
-        .innerHTML = currentTime;
-}
-
-showTime();
-</script>
-<div class="container">
-{{-- <a class="btn btn-sm btnkelas bg-transparent text-light" href="{{ route('kelas1') }}"><- Kembali</a> --}}
-{{-- @if (auth()->user()->level=="guru")
-<a class="btn btn-sm btnkelas bg-transparent text-light" href="/kelas1/ips/tambah">+ Tambah Materi</a>
-@endif --}}
-<button class="btn btn-sm btnkelas bg-transparent text-light" data-bs-toggle="modal" data-bs-target="#exampleModal">? Cari Materi</button>
-{{-- Modal pencarian --}}
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog modal-lg">
-  <div class="modal-content modalbl">
-    <div class="modal-header">
-      <h5 class="modal-title text-dark" id="exampleModalLabel"><i class="fas fa-search"></i> Cari Materi</h5>
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-    </div>
-    <div class="modal-body">
-      <form action="">
-        <div>
-          <input type="text" class="form-control" placeholder="Cari materi disini.." name="keyword"><br>
-          <button class="btn btn-secondary"><i class="fas fa-search"></i> Cari</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-</div>
-</div>
-</div>
-
+@include('template.mapel-materi')
 
 <div class="mainlist mt-4 mb-4">  
   <div class="col-10 container mt-4">
       <div class="list-group list-group-horizontal-sm " id="list-tab" role="tablist">
-          <a class="list-group-item list-group-item-action active fw-bold text-center" href="/detail" aria-controls="list-home"><i class="fas fa-book"></i> Materi</a>
+          <a class="list-group-item list-group-item-action active fw-bold text-center" href="/kelas/materi/{{$mapel->id}}" aria-controls="list-home"><i class="fas fa-book"></i> Materi</a>
           <a class="list-group-item list-group-item-action fw-bold text-center" href="/latihan" aria-controls="list-profile"><i class="fas fa-pencil-alt"></i> Latihan</a>
           <a class="list-group-item list-group-item-action fw-bold text-center" href="/submit" aria-controls="list-profile"><i class="fas fa-calendar-check"></i> Tugas</a>
       </div>
