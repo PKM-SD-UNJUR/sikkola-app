90891c54335a949c57da101cafbca352f925ef06
rizki-P-star24 <83244066+rizki-P-star24@users.noreply.github.com>
2022-07-23 11:11:46 +0700

    repair

--- resources/views/kelas.blade.php
+++ resources/views/kelas.blade.php
@@ -33,14 +33,6 @@
                   </div>
                 </div>
               </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="fw-bold" style="color: #2874A6 ;"><i class="fas fa-book-reader"></i>&nbsp; {{$m->nama}}</h5>
-                  <small class="text-muted caption" style="margin-top: -5px;">{{$m->deskripsi}}
-                  </small><br><br>
-                  <a href="/kelas/materi/{{$m->id}}/{{\Carbon\Carbon::now()->format('m')}}" style="color: #EC7063;" class="materi-link fw-bold">Lihat Materi <i class="fas fa-hand-point-right"></i></a>
-                </div>
-              </div>
             </div>
           </div>
         </div>
